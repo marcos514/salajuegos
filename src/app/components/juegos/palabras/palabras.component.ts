233aefd952d05084ddd3f2242670abdcbdf66063
marcos514 <marmarreyer@gmail.com>
2018-11-04 23:05:25 -0300

    falta api

--- src/app/components/juegos/palabras/palabras.component.ts
+++ src/app/components/juegos/palabras/palabras.component.ts
@@ -21,19 +21,70 @@ export class PalabrasComponent implements OnInit {
     "amor",
     "juego",
     "palabra",
-    "amigo"
+    "amigo",
+    "mesa",
+    "marcos",
+    "rey"
   ];
   palabra="";
   numero;
+  respuesta="";
+
+  aux=100;
+  ganador=false
   constructor() { }
 
   ngOnInit() {
-  this.numero= Math.floor(Math.random() * 8);
+    this.aux=100;
+    this.ganador=false;
+    this.numero= Math.floor(Math.random() * 9);
+    this.respuesta="";
 
-  this.palabra =this.lista[this.numero].split("").sort(function() {return Math.random() - 0.5}).toString();
-  this.tiles=[ {text: this.palabra, cols: 3, rows: 1, color: 'white'},
-]
- 
+    this.palabra =this.lista[this.numero].split("").sort(function() {return Math.random() - 0.5}).toString();
+    this.tiles=[ 
+      {text: "Palabras Magicas", cols: 3, rows: 1, color: 'whitesmoke'},
+      {text: this.palabra, cols: 3, rows: 1, color: '#fffb00'}
+    ]
+    setTimeout(()=>this.timeWhatch(),100)
+
+  }
+
+  timeWhatch() {
+    if(this.aux>0){
+      this.aux -= 0.10;
+      if(this.aux<=0 && ! this.ganador){
+        this.Perder();
+        this.aux=0;
+        return 0;
+      }
+      if(!this.ganador){
+        setTimeout(()=>this.timeWhatch(),100);
+      }
+    }
+  }
+
+  Perder(){
+    this.tiles=[ 
+      {text: "Palabras Magicas", cols: 3, rows: 1, color: 'whitesmoke'},
+      {text: "PERDISTE ERA: "+this.lista[this.numero], cols: 3, rows: 1, color: 'red'}
+    ]
+    this.ganador=true;
+  }
+
+  Comprobar(){
+    if(this.respuesta.toLowerCase()==this.lista[this.numero]){
+      this.Ganar();
+      return 0;
+    }
+    this.Perder()
+  }
+
+  Ganar(){
+    this.tiles=[ 
+      {text: "Palabras Magicas", cols: 3, rows: 1, color: 'whitesmoke'},
+      {text: "Ganaste", cols: 3, rows: 1, color: 'green'}
+    ]
+    this.ganador=true;
 
   }
 
