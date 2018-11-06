9617898c2be1380dab4480cce5c8e5c89fb55110
marcos514 <marmarreyer@gmail.com>
2018-11-05 23:32:56 -0300

    pass

--- src/assets/Api_Juegos/clases/AccesoDatos.php
+++ src/assets/Api_Juegos/clases/AccesoDatos.php
@@ -7,7 +7,7 @@ class AccesoDatos
     private function __construct()
     {
         try { 
-            $this->objetoPDO = new PDO('mysql:host=localhost;dbname=id6888241_sala_juegos;charset=utf8', 'id6888241_marmarreyer', 'lulytam0');
+            $this->objetoPDO = new PDO('mysql:host=localhost;dbname=id6888241_sala_juegos;charset=utf8', 'id6888241_marmarreyer', '');
             } 
         catch (PDOException $e) { 
             print "Error!: " . $e->getMessage(); 
